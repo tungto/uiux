c43c50e238721bcc366f73f71a6c8b2602b4b959
tung.to <toxtung@gmail.com>
2024-10-27 20:47:21 +0700

    init

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+// export default {
+// 	root: path.resolve(__dirname, 'src'),
+// 	server: {
+// 		port: 8080,
+// 		hot: true,
+// 	},
+// };
+
+export default defineConfig({
+	// your config options
+	root: path.resolve(__dirname, 'src'),
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src/index.html'),
+			'~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+		},
+	},
+	server: {
+		port: 8080,
+		hot: true,
+	},
+});
