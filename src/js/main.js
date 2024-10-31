6d4bd75028b7b867df96c432e8b60a499a002c59
Trinh The Long <87536354+SimoneTrinh@users.noreply.github.com>
2024-10-31 10:31:14 +0700

    [FEAT] Add common nav (#2)
    * add using img & draft nav
    
    * complete nav component
    
    * create structure for scss
    
    * remove bootstrap icon
    
    * update package lock

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 // Import our custom CSS
-import '../scss/styles.scss';
+import "../scss/index.scss";
 
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap';
+import * as bootstrap from "bootstrap";
