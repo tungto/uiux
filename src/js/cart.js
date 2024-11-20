aacc0f4335a896d882c9886865ddb8b02284fb6d
Simone Trinh <trinhthelong.bav@gmail.com>
2024-11-20 17:27:34 +0700

    update js for cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -3,6 +3,7 @@ fetch('./pages/components/cart.html')
   .then((html) => {
     document.getElementById('popup-container').innerHTML = html
     initPopup()
+    initCartFunction()
   })
 
 function initPopup() {
@@ -27,4 +28,49 @@ function initPopup() {
       popup.classList.remove('show')
     }
   })
+}
+
+function initCartFunction() {
+  // Get the minus and plus buttons
+  const minusBtn = document.querySelectorAll('.minus')
+  const plusBtn = document.querySelectorAll('.plus')
+  const inputField = document.querySelectorAll('.quantity-input')
+
+  // Decrease input value
+  inputField.forEach((input, index) => {
+    input.value = 1
+    minusBtn[index].addEventListener('click', () => {
+      const currentValue = parseInt(input.value) || 0
+      if (currentValue > 0) {
+        input.value = currentValue - 1
+      }
+    })
+
+    plusBtn[index].addEventListener('click', () => {
+      const currentValue = parseInt(input.value) || 0
+      if (currentValue < 999) {
+        input.value = currentValue + 1
+      }
+    })
+    input.addEventListener('keypress', allowNumbersOnly)
+    input.addEventListener('input', validateInput)
+  })
+}
+
+function allowNumbersOnly(event) {
+  const charCode = event.charCode || event.keyCode
+  // Allow only digits (0–9) and control keys (e.g., backspace)
+  if (charCode < 48 || charCode > 57) {
+    event.preventDefault()
+  }
+}
+
+function validateInput(inputEvent) {
+  const value = parseInt(inputEvent.target.value) || 0
+  console.log(value)
+  if (value < 0) inputEvent.target.value = 0
+  if (value > 999) inputEvent.target.value = 999
+  // console.log(
+  //   `Debug value of the input field was changed. ${inputEvent.target.value}}`
+  // )
 }
